cc49ff09fdff1679c9ac4bfc9adeddfe180c290a
cyZhu <45098659+cyZhu-upup@users.noreply.github.com>
2021-09-23 21:34:38 +0800

    Update baseline.py

--- baseline.py
+++ baseline.py
@@ -22,7 +22,7 @@ transform = T.Compose([
     T.Resize((224,224)),
     T.ToTensor(),
 #     T.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
-    T.Normalize(mean=[0., 0., 0.], std=[1., 1., 1.])
+    T.Normalize(mean=[0.5, 05., 0.5], std=[0.5, 0.5, 0.5])
 ])
 
 def train_one_epoch(model, loader):
